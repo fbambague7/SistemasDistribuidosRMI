4c843e2b8455e2ec487e35e4a9f01c542482240e
JULIAN <JULIAN@DESKTOP-4330O6J>
2018-05-07 17:23:07 -0500

    Consultar mejorado

--- ServidorGestionUsuarios/src/Capa_Negocio/sop_rmi/estaUsuariosImpl.java
+++ ServidorGestionUsuarios/src/Capa_Negocio/sop_rmi/estaUsuariosImpl.java
@@ -14,79 +14,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import Capa_Acceso.Archivo;
+import Capa_Negocio.clasesDAO.UsuarioDAO;
 
 public class estaUsuariosImpl extends UnicastRemoteObject implements estaUsuariosInt {
     
-    FileReader fr;
-    BufferedReader br;
+    
+    private Archivo archivo;
+    private UsuarioDAO userDAO;
     
     public estaUsuariosImpl() throws RemoteException {
         super();
+        userDAO = new UsuarioDAO();
     }
 
     @Override
-    public UsuarioDTO esUsuarioRegistrado(String codigo) throws RemoteException {
-        boolean existe;
-        String ruta = "../src/acceso/usuarios/usuario_"+codigo+".txt"; 
-        String linea,nombres,clave;
-        EnumArea enumArea=null;
-        EnumRol enumRol=null;
-        File fichero = new File(ruta);
-        existe = fichero.exists();
-        UsuarioDTO userDTO = null;
-        String[] row = null;
-        if(existe){
-            System.out.println("Usuario ha sido encontrado");
-            
-            try {
-                while(br.ready()){
-                    linea = br.readLine();
-                    row = linea.split("_");
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(estaUsuariosImpl.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            try {
-                    br.close();
-            } catch (IOException ex) {
-                Logger.getLogger(estaUsuariosImpl.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            nombres = row[1];
-            switch (row[2]) {
-                case "norte":
-                    enumRol = EnumRol.administrativo;
-                    break;
-                case "sur":
-                    enumRol = EnumRol.docente;
-                    break;
-                case "este":
-                    enumRol = EnumRol.estudiante;
-                    break;
-            }
-            clave = row[3];
-            switch (row[4]) {
-                case "norte":
-                    enumArea = EnumArea.norte;
-                    break;
-                case "sur":
-                    enumArea = EnumArea.sur;
-                    break;
-                case "este":
-                    enumArea = EnumArea.este;
-                    break;
-                case "oeste":
-                    enumArea = EnumArea.oeste;
-                    break;
-            }
-            userDTO = new UsuarioDTO(codigo,nombres,clave,enumArea,enumRol);
-        }
-            try {
-                fr = new FileReader(ruta);
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(estaUsuariosImpl.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-        return userDTO;
+    public UsuarioDTO obtenerUsuarioRegistrado(String codigo,String clave, String area) throws RemoteException {
+        System.out.println("Obteniendo el usuario...");
+        return userDAO.consultarUsuario(codigo, clave, area);
     }
 }
-
