74d6c5feb146396588434edb907e22871376bef9
ezEngineeringEzLife <32803807+ezEngineeringEzLife@users.noreply.github.com>
2018-05-06 21:47:02 -0500

    Update estaUsuariosImpl.java

--- ServidorGestionUsuarios/src/Capa_Negocio/sop_rmi/estaUsuariosImpl.java
+++ ServidorGestionUsuarios/src/Capa_Negocio/sop_rmi/estaUsuariosImpl.java
@@ -2,19 +2,91 @@ package Capa_Negocio.sop_rmi;
 
 
 
+import Capa_Negocio.clasesDTO.*;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import Capa_Negocio.clasesDTO.UsuarioDTO;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class estaUsuariosImpl extends UnicastRemoteObject implements estaUsuariosInt {
-
+    
+    FileReader fr;
+    BufferedReader br;
+    
     public estaUsuariosImpl() throws RemoteException {
         super();
     }
 
     @Override
     public UsuarioDTO esUsuarioRegistrado(String codigo) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean existe;
+        String ruta = "../src/acceso/usuarios/usuario_"+codigo+".txt"; 
+        String linea,nombres,clave;
+        EnumArea enumArea=null;
+        EnumRol enumRol=null;
+        File fichero = new File(ruta);
+        existe = fichero.exists();
+        UsuarioDTO userDTO = null;
+        String[] row = null;
+        if(existe){
+            System.out.println("Usuario ha sido encontrado");
+            
+            try {
+                while(br.ready()){
+                    linea = br.readLine();
+                    row = linea.split("_");
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(estaUsuariosImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                    br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(estaUsuariosImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            nombres = row[1];
+            switch (row[2]) {
+                case "norte":
+                    enumRol = EnumRol.administrativo;
+                    break;
+                case "sur":
+                    enumRol = EnumRol.docente;
+                    break;
+                case "este":
+                    enumRol = EnumRol.estudiante;
+                    break;
+            }
+            clave = row[3];
+            switch (row[4]) {
+                case "norte":
+                    enumArea = EnumArea.norte;
+                    break;
+                case "sur":
+                    enumArea = EnumArea.sur;
+                    break;
+                case "este":
+                    enumArea = EnumArea.este;
+                    break;
+                case "oeste":
+                    enumArea = EnumArea.oeste;
+                    break;
+            }
+            userDTO = new UsuarioDTO(codigo,nombres,clave,enumArea,enumRol);
+        }
+            try {
+                fr = new FileReader(ruta);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(estaUsuariosImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        return userDTO;
     }
 }
+
